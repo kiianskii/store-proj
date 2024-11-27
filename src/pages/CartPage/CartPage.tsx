da945e9421151c0c72d071b97881a726389314c2
kiianskii <ankiyanskiy@gmail.com>
2024-11-27 16:02:08 +0200

    added clearing cart

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { selectCart } from "../../redux/auth/slice";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import UserCartItem from "../../components/UserCartItem/UserCartItem";
+import s from "./CartPage.module.css";
+import { AppDispatch } from "../../redux/store";
+import { clearCartThunk } from "../../redux/products/operations";
 
 const CartPage = () => {
   const cartProds = useSelector(selectCart);
+  const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <div>
-      {cartProds.map((prod) => {
-        return <UserCartItem key={prod._id} product={prod} />;
-      })}
+    <div className={s.cart_wrapper}>
+      {cartProds.length > 0 && (
+        <div className={s.btn_wrapper}>
+          <button type="button" onClick={() => dispatch(clearCartThunk())}>
+            Clear all
+          </button>
+        </div>
+      )}
+
+      <div>
+        {cartProds.map((prod) => {
+          return <UserCartItem key={prod._id} product={prod} />;
+        })}
+      </div>
+
+      {cartProds.length < 1 && (
+        <h2 className={s.empty_title}>
+          Your cart is empty! Add some products and come back!
+        </h2>
+      )}
     </div>
   );
 };
